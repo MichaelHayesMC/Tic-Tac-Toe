22864090283a71b16a7d159e583c6385f9c1bc14
Michael Hayes <michael.hayes@marcellin.school.nz>
2026-02-05 14:12:28 +1300

    Current testing Framework Python File

---
+++ code/modules/test.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Tic Tac Toe")
+#root.resizable(False, False)
+
+main_frame = ttk.Frame()
+main_frame.pack()
+
+test_button = ttk.Button(main_frame, text="Testing")
+test_button.grid(padx=10, pady=10, row=0, column=0)
+
+test_button1 = ttk.Button(main_frame, text="Testing1")
+test_button1.grid(padx=10, pady=10, row=1, column=1)
+
+test_button2 = ttk.Button(main_frame, text="Testing2")
+test_button2.grid(padx=10, pady=10, row=2, column=2)
+
+root.mainloop()
