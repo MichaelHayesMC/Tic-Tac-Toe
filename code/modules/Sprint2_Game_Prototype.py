a8009920d7897acaf8fd9142c8ea32f691024634
Michael Hayes <michael.hayes@marcellin.school.nz>
2026-02-15 18:47:07 +1300

    Finished Reordering code in classes to a point where the game works with a prototype main menu

--- code/modules/Sprint2_Game_Prototype.py
+++ code/modules/Sprint2_Game_Prototype.py
@@ -95,19 +95,37 @@ class main_menu(tk.Tk):
 
         style = styling(self)
 
-        pvp_button = ttk.Button(
-            self, 
-            text="Player vs Player", 
-            command=lambda:playable_game(self)
-            )
-        pvp_button.pack()
+        menu_frame = tk.Frame(
+            self,
+            background=("#E1D5E7"),
+            padx=10,
+            pady=10,
+            height=400,
+            width=400
+        )
+        menu_frame.pack()
 
+        pvp_button = ttk.Button(
+            menu_frame, 
+            text="Player vs Player", 
+            command=lambda:playable_game(self),
+            style=("Back.TButton")
+            )
+        pvp_button.grid(row=0, column=0, padx=2, pady=2)
+
+        pve_button = ttk.Button(
+            menu_frame, 
+            text="Player vs Ai", 
+            style=("Back.TButton")
+            )
+        pve_button.grid(row=0, column=1, padx=2, pady=2)
 
 class playable_game(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
+        self.parent = parent
 
-        parent.withdraw()
+        self.parent.withdraw()
         self.title("Game")
         self.geometry("290x400")
         self.resizable(False, False)
@@ -141,77 +159,71 @@ class playable_game(tk.Toplevel):
         self.game_frame = tk.Frame(self.main_frame, background="#f0eaf3", padx=5, pady=5)
         self.game_frame.grid(row=1, column=0, columnspan=3, padx=5, pady=10)
 
-        self.button_grid(self.game_frame)
+        self.button_grid(self.game_frame, self)
 
-        self.restart_button = ttk.Button(self.main_frame, text="Restart", command=game_score_reset, style="Restart.TButton")
+        self.restart_button = ttk.Button(self.main_frame, text="Restart", command=self.game_score_reset, style="Restart.TButton")
         self.restart_button.grid(row=2, column=0, columnspan=2)
 
-        self.back_button = ttk.Button(self.main_frame, text="Back", style="Back.TButton")
+        self.back_button = ttk.Button(self.main_frame, text="Back", style="Back.TButton", command=self.exit)
         self.back_button.grid(row=2, column=1, columnspan=2)
 
-        # Updates score with the string and integer tkinter variables with auto updating properties
-        def score_update(self):
-            self.cross_combined.set(f"{self.cross_score_label.get()}{self.cross_score.get()}")
-            self.tie_combined.set(f"{self.tie_score_label.get()}{self.tie_score.get()}")
-            self.circles_combined.set(f"{self.circles_score_label.get()}{self.circles_score.get()}")
+        self.score_update()
+        game_set()
 
-        # Restart game initiation
-        def game_start(self):
-            # Looks for every game_frame child then deletes them to therefore be replaced
-            for button in self.game_frame.winfo_children():
-                button.destroy()
+    def exit(self):
+        self.after(1, self.parent.deiconify)
+        self.after(1, self.destroy)
+    
+    # Updates score with the string and integer tkinter variables with auto updating properties
+    def score_update(self):
+        self.cross_combined.set(f"{self.cross_score_label.get()}{self.cross_score.get()}")
+        self.tie_combined.set(f"{self.tie_score_label.get()}{self.tie_score.get()}")
+        self.circles_combined.set(f"{self.circles_score_label.get()}{self.circles_score.get()}")
 
-            game_set()
-            button_grid(self.game_frame)
+    # Restart game initiation
+    def game_start(self):
+        # Looks for every game_frame child then deletes them to therefore be replaced
+        for button in self.game_frame.winfo_children():
+            button.destroy()
 
-        # Adds score for the int value for every won game
-        def score_add(label_type):
-            if label_type == "cross":
-                self.cross_score.set(self.cross_score.get() + 1)
-            elif label_type == "tie":
-                self.tie_score.set(self.tie_score.get() + 1)
-            elif label_type == "circles":
-                self.circles_score.set(self.circles_score.get() + 1)
+        game_set()
+        self.button_grid(self.game_frame, self)
 
-            game_start()
-            score_update()
+    # Adds score for the int value for every won game
+    def score_add(self, label_type):
+        if label_type == "cross":
+            self.cross_score.set(self.cross_score.get() + 1)
+        elif label_type == "tie":
+            self.tie_score.set(self.tie_score.get() + 1)
+        elif label_type == "circles":
+            self.circles_score.set(self.circles_score.get() + 1)
 
-        def game_score_reset(self):
-            self.cross_score.set(0)
-            self.tie_score.set(0)
-            self.circles_score.set(0)
+        self.game_start()
+        self.score_update()
 
-            game_start()
-            score_update()
+    def game_score_reset(self):
+        self.cross_score.set(0)
+        self.tie_score.set(0)
+        self.circles_score.set(0)
 
-        # Creates 9 buttons for the 3x3 grid
-        def button_grid(parent_frame):
-            global record_list
-            record_list = {}
-            for id in range(1, 10):
-                button = button_class(id, record_list)
-                button.identification()
+        self.game_start()
+        self.score_update()
 
-# Index of game recording components
-def game_set():
-    global crosses
-    global record_list
-    global x_list
-    global o_list
-    global disable_count
-    disable_count = 0
-    crosses = True
-    record_list = {}
-    x_list = []
-    o_list = []
+    # Creates 9 buttons for the 3x3 grid
+    def button_grid(self, game_frame, parent):
+        global record_list
+        record_list = {}
+        for id in range(1, 10):
+            button = button_class(id, record_list, parent)
+            button.identification(self.game_frame)
 
 # The necessary blueprint to create a 3x3 grid with button widgets
 class button_class:
     # States id var as self property on initiate class (class startup)
-    def __init__(self, id, record_list):
-        super.__init__(self, parent)
+    def __init__(self, id, record_list, parent):
         self.id = id
         self.record_list = record_list
+        self.parent = parent
 
     # Allocates each button to create the 3x3 grid in game_frame widget
     def grid_allocation(self):
@@ -253,10 +265,10 @@ class button_class:
         disable_count += 1
 
         if disable_count != 9:
-            game.after(10, self.win_cond)
+            self.parent.after(10, self.win_cond)
         # Threshold for all buttons to be occupied till able to reset game
         else:
-            game.after(2000, lambda: score_add("tie"))
+            self.parent.after(2000, lambda: self.parent.score_add("tie"))
 
 
     # All possible win scenarios compared with current player input to find winner
@@ -288,10 +300,10 @@ class button_class:
                             for button in self.record_list:
                                 self.record_list[button].configure(state=tk.DISABLED)
                             
-                        game.after(2000, lambda: score_add("cross"))
+                        self.parent.after(2000, lambda: self.parent.score_add("cross"))
                     # Threshold for all buttons to be occupied till able to reset game
                     elif disable_count == 9:
-                        game.after(2000, lambda: score_add("tie"))
+                        self.parent.after(2000, lambda: self.parent.score_add("tie"))
                     
             elif self.button.cget("text") == "⭘":
                 o_list.append(self.id)
@@ -303,26 +315,28 @@ class button_class:
                             self.record_list[button].config(style="OWin.TButton")
                             for button in self.record_list:
                                 self.record_list[button].configure(state=tk.DISABLED)
-                        game.after(2000, lambda: score_add("circles"))
+                        self.parent.after(2000, lambda: self.parent.score_add("circles"))
 
     # Assigns each button with a blank square then follows up to assign each in a 3x3 grid
-    def identification(self):
+    def identification(self, game_frame):
         self.button = ttk.Button(game_frame, command=self.button_func, style="Idle.TButton")
         self.grid_allocation()
         key_name = self.id
         value = self.button
         self.record_list[key_name] = value
 
-def game_wind():
-    # All tkinter widgets instantiated
-
-    #root.after(1000, root.deiconify)
-    #game.after(2000, game.destroy)
-
-    score_update()
-    game_set()
-
-
+# Index of game recording components
+def game_set():
+    global crosses
+    global record_list
+    global x_list
+    global o_list
+    global disable_count
+    disable_count = 0
+    crosses = True
+    record_list = {}
+    x_list = []
+    o_list = []
 
 # Creates loop for tkinter interface
 if __name__ == "__main__":
